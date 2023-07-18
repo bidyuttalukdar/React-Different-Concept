19ba45f4739ecf06cbbc85a96d42924721a5b16f
bidyuttalukdar <bidyut3004@gmail.com>
2023-07-18 16:25:20 +0530

    Grouping the test into TEST SUITES

--- Testing/01-starting-project/src/components/Gettings.test.js
+++ Testing/01-starting-project/src/components/Gettings.test.js
@@ -1,12 +1,14 @@
 import {render, screen} from '@testing-library/react'
 import Gettings from "./Gettings";
-
-test('To check whether specified string is showing or not',()=>{
-    //Arrange
-    render(<Gettings/>)
-    //Act
-
-    //Assert
-    const check = screen.getByText('Bidyut Talukdar',{exact:true})
-    expect(check).toBeInTheDocument();
+//Test Suit when we group together the test
+describe("Greeting Components",()=>{
+    test('To check whether specified string is showing or not',()=>{
+        //Arrange
+        render(<Gettings/>)
+        //Act
+    
+        //Assert
+        const check = screen.getByText('Bidyut Talukdar',{exact:true})
+        expect(check).toBeInTheDocument();
+    })
 })
