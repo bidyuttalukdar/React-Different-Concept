1521d83bb40d51ca8ea28421779c69a9b660e6b1
bidyuttalukdar <bidyut3004@gmail.com>
2023-07-19 14:54:08 +0530

    Tested user interaction and state

--- Testing/01-starting-project/src/components/Gettings.js
+++ Testing/01-starting-project/src/components/Gettings.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React,{ useState,useEffect } from 'react';
 const Gettings = () => {
+    const [buttonClicked, setButtonClicked] = useState(false)
+    const hanbleButtonClick = () =>{
+        setButtonClicked(true)
+    }
     return (
         <>
             <p>Bidyut Talukdar</p>
+            {buttonClicked && <>Changed!</>}
+            {!buttonClicked && <p>It's good to see you</p>}
+            <button onClick={hanbleButtonClick}>Change Text!</button>
         </>
     )
 }
--- Testing/01-starting-project/src/components/Gettings.test.js
+++ Testing/01-starting-project/src/components/Gettings.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import Gettings from "./Gettings";
 //Test Suit when we group together the test
 describe("Greeting Components",()=>{
@@ -10,5 +11,28 @@ describe("Greeting Components",()=>{
         //Assert
         const check = screen.getByText('Bidyut Talukdar',{exact:true})
         expect(check).toBeInTheDocument();
+    })
+
+    test("render good to see you if the button is NOT clicked",()=>{
+    //Arrange
+    render(<Gettings/>)
+    //Act
+
+    //Assert
+    const check = screen.getByText('good to see',{exact:false})
+    expect(check).toBeInTheDocument();
+    })
+
+    test("render Changed! when button is clicked",()=>{
+        //Arrange
+        render(<Gettings/>)
+        //Act
+        const buttonElement  = screen.getByRole('button')
+        userEvent.click(buttonElement)
+        //Assert
+        const check = screen.getByText('Changed!',{exact:false})
+        const isTextChangeCheck = screen.queryByText("good to see",{exact:false}) // to check whether the specified string is present or not we should use queryByText(it will return null in case text is not present there) instead of getByText 
+        expect(check).toBeInTheDocument();
+        expect(isTextChangeCheck).toBeNull();
     })
 })
