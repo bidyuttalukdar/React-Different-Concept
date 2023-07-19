c443454cd3cac0e8b23efbb2251ef13ba307ae9b
bidyuttalukdar <bidyut3004@gmail.com>
2023-07-19 16:03:28 +0530

    Add a child component

---
+++ Testing/01-starting-project/src/components/Output.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Output = props =>{
+    return <>{props.children}</> 
+    // if component is not so complex we do not need to separate the test cases parent component test cases are enough
+}
