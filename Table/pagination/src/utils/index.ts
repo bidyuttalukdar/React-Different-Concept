9e8051c44f03a0b586fa3b565688483bd7a25914
bidyuttalukdar <bidyut3004@gmail.com>
2023-09-01 15:59:31 +0530

    Changed util function for pagination

--- Table/pagination/src/utils/index.ts
+++ Table/pagination/src/utils/index.ts
@@ -17,15 +17,18 @@ export const returnPaginationrange = (totalPage,page,limit,siblings) => {
 
     if(!showLeftDots && showRightDots){
         let leftItemsCount = 3+2*siblings
-        let leftRange = _.range(1,leftItemsCount+1);
+        let range = _.range(1,leftItemsCount+1);
+        let leftRange = range.filter((value:number)=>value!=totalPage && value>0)
         return [...leftRange," ...",totalPage]
     }else if(!showRightDots && showLeftDots){
         let RightItemsCount = 3+2*siblings
-        let rightRange = _.range(totalPage - RightItemsCount+1,totalPage);
+        let range = _.range(totalPage - RightItemsCount+1,totalPage+1);
+        let rightRange = range.filter((value:number)=>value>1)
         return [1,'... ',...rightRange]
     }else{
         let middleRange = _.range(leftSiblingsIndex,rightSiblingsIndex+1);
-        return [1,'... ',...middleRange,'... ',totalPage]
+        // let middleRange = range.filter((value:number)=>value!=1)
+        return ['... ',...middleRange,' ...']
     }
     
 }
